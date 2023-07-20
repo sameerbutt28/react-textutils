8f1e7b3e2b1ff639a68be1d7de483537a557ae9e
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-07-21 02:16:57 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar'
+import Textform from './components/Textform';
+import About from './components/About'
 
 function App() {
   return (
-   <Navbar
+    <div>
+       <Navbar
    title='TextUtils'
+   about= 'About TextUtils'
    />
+   <div className='container my-3'>
+{/* <Textform
+heading = 'Enter text to analyze below'
+/> */}
+<About/>
+   </div>
+   
+    </div>
+  
   );
 }
 
