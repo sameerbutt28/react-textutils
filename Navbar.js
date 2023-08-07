df31898fab8b16da1b8bc8c9f2b66ae5157c0482
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-08-08 00:52:21 +0500

    Add files via upload

--- Navbar.js
+++ Navbar.js
@@ -1,30 +1,31 @@
 import PropTypes from 'prop-types'
-
+import { Link } from 'react-router-dom';
 export default function Navbar(props){
 return (
     
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}> 
+    {/* backticks are used to use templete literals in the tring and curly braces shows the usesage of javascript */}
 <div className="container-fluid">
-  <a className="navbar-brand" href="/">{props.title}</a>
+  <Link className="navbar-brand" to="/textform">{props.title}</Link>
   <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
       <li className="nav-item">
-        <a className="nav-link active" aria-current="page" href="/">Home</a>
+        <Link className="nav-link active" aria-current="page" to="/textform">Home</Link>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="/">{props.about}</a>
+        <Link className="nav-link" to="/about">{props.about}</Link>
       </li>
      
       
   
     </ul>
-    <form className="d-flex" role="search">
-      <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button className="btn btn-outline-primary" type="submit">Search</button>
-    </form>
+    <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}` }>
+  <input onClick={props.toogleMode}  className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+  <label  onClick={props.toogleMode} className="form-check-label" htmlFor="flexSwitchCheckDefault"> {props.labelText}</label>
+</div>
   </div>
 </div>
 </nav>
